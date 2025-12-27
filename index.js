339ee806d4788e04e9529950dce7d612ad7f161d
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-12-27 16:13:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Backend is running 🚀' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
